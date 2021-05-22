a4d3725a3b330aeb03fc66c1c7ded315f423f69e
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-05-22 03:33:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './sass/main.scss';
+
+import fetchCountries from './js/fetchCountries.js';
+import { renderMarckup, resetMarckup } from './js/marckup.js';
+import { onError, onTooManyMatches } from './js/notifications.js';
+import debounce from 'lodash.debounce';
+import refs from './js/refs.js';
+
+refs.searchQuery.addEventListener('input', debounce(onSearch, 700));
+
+function validation(data) {
+  if (data.length > 10) {
+    return onTooManyMatches();
+  };
+
+  return data;
+}
+
+function onSearch(e) {
+  const query = e.target.value;
+
+  if (!query) {
+    resetMarckup();
+    return;
+  };
+
+  fetchCountries(query)
+    .then(validation)
+    .then(renderMarckup)
+    .catch(onError);
+};
